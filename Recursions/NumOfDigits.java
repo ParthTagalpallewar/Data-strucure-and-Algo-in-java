db606f762470e5d73e9c1e816775f4e107c9fd47
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2022-01-12 23:54:10 +0530

    Recursion question Count number of digits

---
+++ Recursions/NumOfDigits.java
@@ -0,0 +1,23 @@
+package Recursions;
+
+public class NumOfDigits {
+    
+    public static void main(String[] args) {
+
+        int digits = countDigits(10);
+        System.out.println("Number of digits are " + digits);
+
+
+    }
+
+    public static int countDigits(int num){
+        int digits = 1;
+        int division = num / 10;
+
+        if(division == 0) return 1;
+
+        return digits + countDigits(division);
+
+
+    }
+}
