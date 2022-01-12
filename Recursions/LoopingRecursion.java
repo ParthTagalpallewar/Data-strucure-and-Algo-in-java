304b60e8c019f931e3264553f45d0eb08091b304
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2022-01-12 19:02:15 +0530

    Recursion question loop using recursion

---
+++ Recursions/LoopingRecursion.java
@@ -0,0 +1,32 @@
+package Recursions;
+
+public class LoopingRecursion {
+    public static void main(String[] args) {
+        // printNum(10, 1);
+        printMe(5);
+    }
+
+    public static int printNum(int num, int current){
+
+        System.out.println(current);
+
+        if(num == current){
+            return current;
+        }
+
+        return printNum(num, current += 1);
+        
+        
+    }
+
+    public static void printMe(int limit){
+
+        if(limit > 0){
+            printMe(limit - 1);
+            System.out.println(limit);
+        }
+
+        return;
+
+    }
+}
