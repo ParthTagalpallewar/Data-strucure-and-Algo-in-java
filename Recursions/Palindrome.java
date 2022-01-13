1508e6d50eb9b4c3ad2fe379076871450744bd29
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2022-01-13 16:36:20 +0530

    Palindrome check completed

---
+++ Recursions/Palindrome.java
@@ -0,0 +1,24 @@
+package Recursions;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String data = "abcddcba";
+        Boolean isPalindrome = checkPalindrome(data, 0, data.length()-1);
+        System.out.println(isPalindrome);
+    }
+
+    public static Boolean checkPalindrome(String data, int start, int end){
+
+        int n = data.length();
+
+        if(n % 2 == 0 && start > end) return true;
+        
+        if(n % 2 != 0 && start == end) return true;
+
+        if(data.charAt(start) != data.charAt(end)) return false;
+
+        return checkPalindrome(data, start+1, end-1);
+
+
+    }
+}
