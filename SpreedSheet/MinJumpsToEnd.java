573ce443377bbe1720544970fe467527e0d3d950
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2022-03-01 09:11:45 +0530

    Min Jumps to reach end

--- SpreedSheet/MinJumpsToEnd.java
+++ SpreedSheet/MinJumpsToEnd.java
@@ -1,51 +1,61 @@
 class MinJumpsToEnd{
     public static void main(String[] args) {
-        int[] arr = { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
+        int[] arr = {1, 4, 3, 2, 6, 7};
+        //  int[] arr = {2, 3, 1, 1, 2, 4, 2, 0, 1, 1};
+        //  int[] arr = {1, 4, 3, 2, 6, 7};
 
         int minJumps = minJumpsToEnd(arr);
         System.out.println("Minimum number of jumps to reach end is " + minJumps);
     }
 
     public static int minJumpsToEnd(int[] arr){
-         
-        int curr_idx = 0;
-        
-        int max_num;
-        int max_index;
-        int count = 1;
-        
-        while(curr_idx < arr.length){
+       
+        int minJumps = 0;
+        int currentIndex = 0;
+
+        while(currentIndex < arr.length-1){
             
-            int curr_val = arr[curr_idx];
-            
-            max_num = 0;
-            max_index = 0;
-            
-            if(curr_idx + curr_val >= arr.length){
-                count += 1;
+            int maxJumps = arr[currentIndex];
+
+            if(currentIndex + maxJumps >= arr.length-1){
+                minJumps += 1;
                 break;
             }
-            
-            
-            for(int i = curr_idx; i < curr_idx + curr_val; i++){
-                
-                if(arr[i] > max_num){
-                    max_num = arr[i];
-                    max_index = i;
-                  
-                    count += 1;
-                    
-                    System.out.println(max_num);
-                }
-                
-                if(i >= arr.length) break;
-                
-            }
-            
-            curr_idx = max_index;
-            
+
+            int nextJump = getCurrentJump(arr, currentIndex);
+
+            if(nextJump == -1) return -1;
+
+            minJumps += 1;
+            currentIndex = nextJump;
+
+
         }
-        
-        return count;
+
+        return minJumps;
+       
     }
+
+    public static int getCurrentJump(int[] arr, int currentIndex){
+        int maxJumps = arr[currentIndex];
+
+        int nextJump = 0;
+        int nextJumpIndex = 0;
+
+
+        for(int i = currentIndex + 1; i <= currentIndex + maxJumps; i++){
+            if(arr[i] != 0 && arr[i] + i >= nextJump) {
+                nextJump = arr[i] + i;
+                nextJumpIndex = i;
+            }
+
+            // System.out.println(arr[i]);
+        }
+
+        // System.out.println();
+
+        return (nextJumpIndex == 0) ? -1 : nextJumpIndex;
+    }
+
+    
 }
