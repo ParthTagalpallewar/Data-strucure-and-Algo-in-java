6ce19ac1fa9b7bc4ac95e73b60f0cb1f74d4571e
Parth Tagalpallewar <parthtagalpallewar123@gamil.com>
2022-04-18 07:30:14 +0530

    send to check

--- PairPain.java
+++ PairPain.java
@@ -1,18 +1,29 @@
 import java.util.HashMap;
-import java.util.Map;
 
 public class PairPain {
     
     public static void main(String[] args) {
         
-        int[] arr = {1, 1, 2, 2, 3, 3};
+        int[] arr = {2, 5, 6, 2, 7, 8, 2, 9, 6, 2};
+
+        int ones = 0;
+        int twos = 0;
+
+        for(int num: arr){
+            if(num == 1) ones++;
+
+            else if(num == 2) twos++;
+        }
+
 
         int com_pairs = findPairs1(arr);
-        int pairs = findPairs2(arr);
+        int pairs = findPairs2(arr, ones, twos);
 
         System.out.println("num of pairs n2 " + com_pairs);
         System.out.println("num of pairs " + pairs);
     }
+
+    
 
     private static int findPairs1(int[] arr) {
         
@@ -33,53 +44,26 @@ public class PairPain {
 
     }
 
-    private static int findPairs2(int[] arr){
-
-        HashMap<Integer, Integer> map = getMap(arr);
+    private static int findPairs2(int[] arr, int ones, int twos){
 
         int pairs = 0;
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()){
-
-            if(entry.getKey() == 1) pairs += one_pairs(arr, map.get(1));
-
-            else pairs += ((entry.getValue() * (entry.getValue()-1))/2);
+        int ones_count = ones;
+        int start = arr.length-1; 
+        
+        while(ones_count-- > 0){
+            pairs += start--;
         }
 
+        pairs += (twos * (twos - 1) / 2);
+       
+       
         return pairs;
         
 
     }
 
-    private static int one_pairs(int arr[], int one_count){
-        int pairs = 0;
+   
 
-        int n = arr.length - 1;
-        while (n >= arr.length - one_count) {
-            pairs += n;
-            n -= 1;
-        }
-
-        return pairs;
-    }
-
-    private static HashMap<Integer, Integer> getMap(int[] arr){
-
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-        for (int i = 0; i < arr.length; i++) {
-            
-            int num = arr[i];
-
-            if(map.containsKey(num)) map.put(num, map.get(num)+1);
-
-            else map.put(num, 1);
-
-        }
-
-        return map;
-
-    }
-
+  
     
 }
